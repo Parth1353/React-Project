88d7f06f3ce33d86826552626594777624bb1b46
Parth Saini <parthsaini13@gmail.com>
2023-10-31 16:47:16 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React from "react";
-import "./App.css"; // Ensure that your CSS file path is correct
+import React, { useState } from "react";
+import "./index.css"; // Ensure that your CSS file path is correct
 
 export default function App() {
   return (
@@ -19,13 +19,14 @@ function Headers() {
 }
 
 function Main() {
+  const[description,setDescription]=useState("");
   return (
     <div className="form">
       <form className="realform">
         <h1 className="form-text">Add Task Here</h1>
         <div className="input1">
           <div class="label-float">
-            <input type="text" placeholder=" " required />
+            <input type="text" placeholder=" " required onChange={(e) => setDescription(e.target.value)}  />
             <label>Task</label>
           </div>
 
