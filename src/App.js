a4382992149b217a38f47fa5ea6510e3910f4ad6
Parth Saini <parthsaini13@gmail.com>
2023-11-01 01:13:32 +0530

    please

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
 import React, { useState } from "react";
-import "./index.css"; // Ensure that your CSS file path is correct
-
+import "./index.css";
+const item = [
+  {
+    id: 1,
+    description: "Book a Train Ticket",
+    date:22/12/2023
+  },
+  {
+    id: 2,
+    description: "Book a bus Ticket",
+    date:22/12/2023
+  },
+];
 export default function App() {
   return (
     <div>
@@ -20,12 +31,11 @@ function Headers() {
 
 function Main() {
   const [description, setDescription] = useState("");
-
+  const [date, setDate] = useState("");
   function handleSub(e) {
     e.preventDefault();
-    // Perform actions with the form data (e.g., submit to a database)
     console.log("Task description:", description);
-    // Reset the description state or perform any other action after form submission
+    console.log("Task date:", date);
     setDescription("");
   }
 
@@ -44,7 +54,11 @@ function Main() {
             />
             <label>Task</label>
           </div>
-          <input type="date" id="date"></input>
+          <input
+            type="date"
+            id="date"
+            onChange={(e) => setDate(e.target.value)}
+          ></input>
         </div>
         <button type="submit" className="button">
           Submit
