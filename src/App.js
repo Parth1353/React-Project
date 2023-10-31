a6b715ba10f482c071c28e7e8b97b9f637415914
Parth Saini <parthsaini13@gmail.com>
2023-10-31 18:03:37 +0530

    please add

--- src/App.js
+++ src/App.js
@@ -19,20 +19,36 @@ function Headers() {
 }
 
 function Main() {
-  const[description,setDescription]=useState("");
+  const [description, setDescription] = useState("");
+
+  function handleSub(e) {
+    e.preventDefault();
+    // Perform actions with the form data (e.g., submit to a database)
+    console.log("Task description:", description);
+    // Reset the description state or perform any other action after form submission
+    setDescription("");
+  }
+
   return (
     <div className="form">
-      <form className="realform">
+      <form className="realform" onSubmit={handleSub}>
         <h1 className="form-text">Add Task Here</h1>
         <div className="input1">
-          <div class="label-float">
-            <input type="text" placeholder=" " required onChange={(e) => setDescription(e.target.value)}  />
+          <div className="label-float">
+            <input
+              type="text"
+              placeholder=" "
+              required
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+            />
             <label>Task</label>
           </div>
-
           <input type="date" id="date"></input>
         </div>
-        <button className="button">Submit</button>
+        <button type="submit" className="button">
+          Submit
+        </button>
       </form>
     </div>
   );
